b0b74d1db9f5678d05d1ba6c3352ca699142e691
thushan-123 <thushanmadusanka456@gmail.com>
2026-01-05 00:06:42 +0530

    create byte packets

--- icmp/Icmp.cpp
+++ icmp/Icmp.cpp
@@ -15,6 +15,30 @@ class ICMP {
         // std::array<std::byte, 64>   build () {
         //     return type + code + checksum + identifier[:2]+ payload[:56]
         // }
+        std::array<std::byte , 64> build () const {
+
+            std::array<std::byte, 64> packet{};
+
+            packet[0] = type;
+            packet[1] = code;
+
+            // checksum
+            packet[2] = std::byte(checksum >> 8);
+            packet[3] = std::byte(checksum & 0xFF);
+
+            // identifier
+            packet[4] = std::byte(identifier >> 8);
+            packet[5] = std::byte(identifier & 0xFF);
+
+            // seq
+            packet[6] = std::byte{0};
+            packet[7] = std::byte{0};
+
+            // payload
+            for(int i =0 ; i < payload.size(); i++){
+                packet[8 + i] = payload[i];
+            }
+        }
 
 
 };
