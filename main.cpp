90375f1f16f826a580b92117dcabb10974787d72
thushan-123 <thushanmadusanka456@gmail.com>
2026-01-04 23:10:11 +0530

    check argument

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-int main () {
+int main (int argc, char *argv[]) {
+
+    // check argument count
+    if ( argc != 2 ){
+        std::cerr << "usage " << argv[0] << " host \n";
+    }
 
     return 0;
 }
