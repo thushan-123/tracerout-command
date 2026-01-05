eacc7ea7ee6446672867ee9c554fa452f1450f5e
thushan-123 <thushanmadusanka456@gmail.com>
2026-01-05 06:53:02 +0530

    resolve domain name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netdb.h>
+
+
+#define MAX_HOPS 64
+#define TIME_OUT 2
 
 int main (int argc, char *argv[]) {
 
@@ -8,5 +20,19 @@ int main (int argc, char *argv[]) {
         return 1;
     }
 
+    // resolve host nam
+    struct hostent *host = gethostbyname(argv[1]);
+    if (host == NULL) {
+        std::cout << "can't resolve domain name" << std::endl;
+        return 1;
+    }
+
+    sockaddr_in socket{};
+    socket.sin_family = AF_INET;
+
+
+
     return 0;
+
+
 }
