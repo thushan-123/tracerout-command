7f63532a20623befc3b1ed8a1444813ef213abe3
thushan-123 <thushanmadusanka456@gmail.com>
2026-01-05 11:18:04 +0530

    move tcp udp because tcpicmp  bloks some hops

--- main.cpp
+++ main.cpp
@@ -4,29 +4,27 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
+#include <netinet/ip_icmp.h> 
+#include <netinet/udp.h>
 #include <sys/socket.h>
 #include <sys/time.h>
 #include <netdb.h>
 #include <chrono>
 
-#include "icmp/icmp.h"
-
 #define MAX_HOPS 64
-#define TIME_OUT 2
+#define TIME_OUT 3
+#define BASE_DST_PORT 33434
 
-int main (int argc, char *argv[]) {
-
-    // check argument count
-    if ( argc != 2 ){
-        std::cout<< "usage " << argv[0] << " < host > " << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <host>\n";
         return 1;
     }
 
-    // resolve host nam
-    struct hostent *host = gethostbyname(argv[1]);
-    if (host == NULL) {
-        std::cout << "can't resolve domain name" << std::endl;
+    //resolve host
+    hostent* host = gethostbyname(argv[1]);
+    if (!host) {
+        std::cerr << "Cannot resolve host\n";
         return 1;
     }
 
@@ -34,83 +32,78 @@ int main (int argc, char *argv[]) {
     dest.sin_family = AF_INET;
     memcpy(&dest.sin_addr, host->h_addr, host->h_length);
 
-    char  target_ip[INET_ADDRSTRLEN];
-
+    char target_ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &dest.sin_addr, target_ip, sizeof(target_ip));
+    std::cout << "Traceroute to " << argv[1] << " (" << target_ip << "), "
+              << MAX_HOPS << " max hops\n\n";
 
-    std::cout << "Traceroute to " << argv[1] << "(" << target_ip << ") " << "64 max hops" <<std::endl;
-    std::cout << std::endl;
+    
+    int icmp_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (icmp_sock < 0) { perror("icmp socket"); return 1; }
 
-    int sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-    if (sock < 0){
-        std::cout<< "socket ERROR" << std::endl;
-        return 1;
-    }
-
-    //time out
     timeval timeout{};
     timeout.tv_sec = TIME_OUT;
-    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+    setsockopt(icmp_sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
 
+    for (int ttl = 1; ttl <= MAX_HOPS; ttl++) {
+        // UDP socket 
+        int udp_sock = socket(AF_INET, SOCK_DGRAM, 0);
+        if (udp_sock < 0) { perror("udp socket"); return 1; }
 
-    for (int ttl = 1; ttl <= MAX_HOPS; ttl++){
-        setsockopt(sock, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
+        //ttl
+        setsockopt(udp_sock, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
 
-        // create icmp packet
-        ICMP icmp{};
-
-        icmp.type = 0x08;
-        icmp.code = 0x00;
-        icmp.un.echo.id = getpid();
-        icmp.un.echo.sequence = ttl;
-        icmp.payload.fill(0xAA);
-        icmp.checksum = 0;
-
-        auto packet = icmp.build();
-
-        icmp.checksum = icmp.icmpChecksum(reinterpret_cast<const u_int16_t *>(packet.data()), packet.size());
-
-        packet = icmp.build();
+        
+        int dst_port = BASE_DST_PORT + ttl;
+        dest.sin_port = htons(dst_port);
 
         auto start = std::chrono::high_resolution_clock::now();
 
-        sendto(sock, packet.data() , packet.size(), 0, (sockaddr * ) &dest, sizeof(dest));
+        //send UDP packet
+        sendto(udp_sock, "", 0, 0, (sockaddr*)&dest, sizeof(dest));
+        close(udp_sock);  // No need to keep it open
 
-        char recvbuffer[1024];
+        // Receive ICMP reply
+        char recvbuf[1024];
         sockaddr_in reply{};
         socklen_t len = sizeof(reply);
 
-        int n = recvfrom(sock , recvbuffer, sizeof(recvbuffer), 0, (sockaddr*)&reply , &len);
+        int n = recvfrom(icmp_sock, recvbuf, sizeof(recvbuf), 0,
+                         (sockaddr*)&reply, &len);
 
         auto end = std::chrono::high_resolution_clock::now();
+        double rtt = std::chrono::duration<double, std::milli>(end - start).count();
+
+        std::cout << ttl << "  ";
 
         if (n < 0) {
-            std::cout << ttl << "  *\n";
+            std::cout << "*\n";
             continue;
         }
 
-        auto rtt = std::chrono::duration<double, std::milli>(end - start).count();
-
         char hop_ip[INET_ADDRSTRLEN];
         inet_ntop(AF_INET, &reply.sin_addr, hop_ip, sizeof(hop_ip));
+        std::cout << hop_ip << "  " << rtt << " ms";
 
-        std::cout << ttl << "  " << hop_ip
-                  << "  " << rtt << " ms\n";
+    
+        struct ip* ip_hdr = (struct ip*)recvbuf;
+        int ip_hdr_len = ip_hdr->ip_hl * 4;
+        
+        struct icmp* icmp_header = (struct icmp*)(recvbuf + ip_hdr_len);
 
-        struct ip *ip_hdr = (struct ip*)recvbuffer;
-        int ip_header_len = ip_hdr->ip_hl * 4;
+        
+        if (icmp_header->icmp_type == ICMP_UNREACH && 
+            icmp_header->icmp_code == ICMP_UNREACH_PORT) {
 
-        struct icmp *icmp_hdr = (struct icmp *)(recvbuffer + ip_header_len);
-        if (icmp_hdr->icmp_type == ICMP_ECHOREPLY) {
+            std::cout << "  (destination reached)";
+            std::cout << "\n";
             break;
         }
 
-
-
+        std::cout << "\n";
     }
-    close(sock);
 
+    close(icmp_sock);
     return 0;
-
-
 }
+
